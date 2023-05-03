2c17a33ddf6a081c6dda6d8a0c46e4fbb21d5701
Mateusz Majewski <mat.maj1992@gmail.com>
2023-05-03 15:44:23 +0200

    dark/light theme switcher project finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme
+}
+
 function App() {
-  return <main>
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleHandler = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  }
+
+  useEffect(() => {   
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
+  return (
+  <main>
     <nav>
       <div className="nav-center">
         <h1>overreacted</h1>
-        <button className="btn">toggle</button>
+        <button className="btn" onClick={toggleHandler}>toggle</button>
       </div>
     </nav>
     <section className="articles">
@@ -16,6 +40,7 @@ function App() {
       })}
     </section>
   </main>
+  )
 }
 
 export default App
